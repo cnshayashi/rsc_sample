a609157e29134f3e6ef0cd9f79d8ef57971c2350
cnshayashi <134460165+cnshayashi@users.noreply.github.com>
2023-07-12 09:31:46 +0900

    Update page.tsx

--- app/practice/composition/page.tsx
+++ app/practice/composition/page.tsx
@@ -1,9 +1,33 @@
+import { css } from "@/styled-system/css";
 import React from "react";
+import Check from "./_components/Check";
+import Button from "./_components/Button";
+import { ContextProvider } from "./checkdContext";
 
-export default function CompositionPage() {
+export default function BadPage() {
   return (
-    <>
-      composition page
-    </>
+    <ContextProvider>
+      <div
+        className={css({
+          margin: "10px"
+        })}
+      >
+        <h3
+          className={css({
+            fontSize: "18px",
+            fontWeight: "bold",
+            marginBottom: "5px"
+          })}
+        >
+          利用規約
+        </h3>
+        <p>
+          利用規約です。
+        </p>
+      </div>
+      <Check />
+      <hr />
+      <Button />
+    </ContextProvider>
   );
 }
